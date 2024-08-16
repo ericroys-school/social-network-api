e2509f49a701f22c715ae8a7d952c53ab585959d
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-16 16:32:32 -0500

    fix: refactor the date formatter
    add time to output, pad the digits to 2 spaces where appropriate

--- models/util.js
+++ models/util.js
@@ -1,9 +1,14 @@
 /**
  * Formats a date into MM/DD/YYYY
- * @param {createdAt} d 
- * @returns 
+ * @param {createdAt} d
+ * @returns
  */
 export function formatDate(createdAt) {
-    const d = new Date(createdAt);
-    return `${d.getMonth()}/${d.getDay()}/${d.getFullYear()}`;
+  const d = new Date(createdAt);
+  return `${pad(d.getMonth())}/${pad(d.getDay())}/${d.getFullYear()}  ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+}
+
+//make sure segment has 2 chars or it just looks silly :-)
+const pad= (num)=>{
+    return num.toString().padStart(2,"0");
 }
